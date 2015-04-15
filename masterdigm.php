9499ea5d88b7fcd566f097e9f8b9f97c60bff0f1
allan.casilum <allan.casilum@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-15 03:54:39 +0000

    updated masterdigm description and uriadded hyperlink
    git-svn-id: https://plugins.svn.wordpress.org/wp-real-estate-property-listing-crm/trunk@1135095 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- masterdigm.php
+++ masterdigm.php
@@ -16,7 +16,7 @@
  * @wordpress-plugin
  * Plugin Name:       Masterdigm API
  * Plugin URI:        http://www.masterdigm.com/realestatewordpressplugin
- * Description:       Used by Professional Real Estate companies around the world, Masterdigm Real Estate WP plugin will help you build your real estate website.  To get started: 1) Click the "Activate" link to the left of this description, 2) Sign up for a Masterdigm API Key, 3) Go to your left menu:  Masterdigm >> Masterdigm and add your key, token and ID.  Go to this page to learn exactly how: http://www.masterdigm.com/realestatewordpressplugin
+ * Description:       Used by Professional Real Estate companies around the world, Masterdigm Real Estate WP plugin will help you build your real estate website.  To get started: 1) Click the "Activate" link to the left of this description, 2) Sign up for a Masterdigm API Key, 3) Go to your left menu:  Masterdigm >> Masterdigm and add your key, token and ID.  Go to this page to learn exactly how: <a href="http://www.masterdigm.com/realestatewordpressplugin" target="_blank">http://www.masterdigm.com/realestatewordpressplugin</a>
  * Version:           1.0.0
  * Author:            Masterdigm
  * Author URI:        http://masterdigm.com/
