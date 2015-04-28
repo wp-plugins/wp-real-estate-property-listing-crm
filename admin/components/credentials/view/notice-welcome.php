d47483d7b73ee8867827c62095b3652188aff202
allan.casilum <allan.casilum@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-28 07:28:51 +0000

    updated v1.4.3 readme and notice
    git-svn-id: https://plugins.svn.wordpress.org/wp-real-estate-property-listing-crm/trunk@1147814 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/components/credentials/view/notice-welcome.php
@@ -0,0 +1,5 @@
+<div class="md-notice">
+	<div class="about-text md-notice-text">
+		<p>An All-in-One Real Estate Plugin Solution for Brokerages and Agents Integrated with a High Powered Real Estate CRM Solution.</p>
+	</div>
+</div>
