5111e17d0dd50240fbd3d2d3917354a90906817b
allan.casilum <allan.casilum@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-28 07:28:51 +0000

    updated v1.4.3 readme and notice
    git-svn-id: https://plugins.svn.wordpress.org/wp-real-estate-property-listing-crm/tags/1.4.3@1147814 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/components/credentials/view/notice.php
@@ -0,0 +1,5 @@
+<div class="md-notice">
+	<div class="update-nag">
+		<p>It seems you have no API Key, please <a href="http://www.masterdigm.com/pricing/" target="_blank">click here to get api key</a></p>
+	</div>
+</div>
