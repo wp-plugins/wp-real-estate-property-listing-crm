ca1a6dfc08430906c5895f1d2ff5507390ea851c
allan.casilum <allan.casilum@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-06 05:29:36 +0000

    add crm class
    git-svn-id: https://plugins.svn.wordpress.org/wp-real-estate-property-listing-crm/trunk@1154294 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ public/mls/template/single/partials/carousel/html-galleria.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Template Name: Carousel - Single, Galleria Script
+*/
+?>
+<style>
+#galleria{height:450px;}
+/*#galleria .galleria-image-nav-left, body .galleria-image-nav-right {
+    background-image: url("<?php echo PLUGIN_PUBLIC_URL;?>galleria/themes/classic/arrows.png") !important;
+}*/
+.galleria-stage{height:85% !important;}
+</style>
+<div class="single-property-photos single-property-slider">
+	<div class="galleria crm-galleria" id="galleria">
+		<?php if( count(get_single_property_photos()) > 0 ){ ?>
+			<?php foreach( get_single_property_photos() as $photo ){ ?>
+				<a href="<?php echo $photo->url;?>">
+					<img
+						src="<?php echo $photo->url;?>",
+						data-big="<?php echo $photo->url;?>"
+						data-title=""
+						data-description=""
+					>
+				</a>
+			<?php } ?>
+		<?php } ?>
+	</div>
+</div>
+<script>
+jQuery(document).ready(function($){
+	// Load the classic theme
+	Galleria.loadTheme('<?php echo PLUGIN_PUBLIC_URL . 'galleria/themes/classic/galleria.classic.min.js';?>');
+	// Initialize Galleria
+	Galleria.configure({
+		lightbox: true,
+		responsive:true,
+		imageCrop: true,
+		preload:2,
+		dummy: '<?php echo PLUGIN_ASSET_URL . 'house.png';?>',
+	});
+	Galleria.run('#galleria');
+});
+</script>
