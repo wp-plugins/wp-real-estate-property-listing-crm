54f64a73bda54da03423ab4ec3fe8037258ddbf3
allan.casilum <allan.casilum@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-11 14:20:57 +0000

    update pinterest share media
    git-svn-id: https://plugins.svn.wordpress.org/wp-real-estate-property-listing-crm/tags/2.8.5@1157987 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/md-template.php
+++ includes/md-template.php
@@ -18,3 +18,12 @@ function md_global_carousel_template($atts){
 function next_prev(){
 	\Next_Prev::get_instance()->display();
 }
+function display_map_single($atts){
+	\MD_Template::get_instance()->single_map($atts);
+}
+function display_walkscore_single($atts){
+	\MD_Template::get_instance()->single_walkscore($atts);
+}
+function display_photos_single($atts){
+	\MD_Template::get_instance()->single_photos($atts);
+}
--- includes/mls/init.php
+++ includes/mls/init.php
@@ -1,16 +1,19 @@
 <?php
 // mls
+require_once( plugin_dir_path( __FILE__ ) . 'class-mls.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'class-property.php' );
+//refactor
 require_once( plugin_dir_path( __FILE__ ) . 'class-masterdigm-mls.php' );
 //account
 require_once( plugin_dir_path( __FILE__ ) . 'class-account.php' );
 add_action( 'plugins_loaded', array( '\mls\AccountEntity', 'get_instance' ) );
 // class for properties
-require_once( plugin_dir_path( __FILE__ ) . 'properties/class-mls-properties.php' );
+//require_once( plugin_dir_path( __FILE__ ) . 'properties/class-mls-properties.php' );
 // class for property entity
 require_once( plugin_dir_path( __FILE__ ) . 'properties/class-mls-property-entity.php' );
 // view
-require_once( plugin_dir_path( __FILE__ ) . 'class-layout-property.php' );
-add_action( 'plugins_loaded', array( '\mls\Layout_Property', 'get_instance' ) );
+require_once( plugin_dir_path( __FILE__ ) . 'class-template-property.php' );
+add_action( 'plugins_loaded', array( '\mls\Template_Property', 'get_instance' ) );
 // search by
 require_once( plugin_dir_path( __FILE__ ) . 'class-searchby-properties.php' );
 add_action( 'plugins_loaded', array( '\mls\MD_Searchby_Property', 'get_instance' ) );
