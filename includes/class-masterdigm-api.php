960ef7c4e5902aa9b8ff6463a232083b34b217a7
allan.casilum <allan.casilum@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-29 02:51:06 +0000

    fix enfold support notice error, add masterdigm-style.css, v3.8.11
    git-svn-id: https://plugins.svn.wordpress.org/wp-real-estate-property-listing-crm/trunk@1169800 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class-masterdigm-api.php
+++ includes/class-masterdigm-api.php
@@ -99,7 +99,7 @@ class Masterdigm_API {
 	public function __construct() {
 
 		$this->plugin_name = 'masterdigm-api';
-		$this->version = '3.8.10';
+		$this->version = '3.8.11';
 
 		$this->load_dependencies();
 		$this->set_locale();
