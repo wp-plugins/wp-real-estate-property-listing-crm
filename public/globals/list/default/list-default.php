f22307775607ba9a79a25bd7366d50fc7ac5741f
allan.casilum <allan.casilum@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-02 14:11:55 +0000

    v3.10.16
    git-svn-id: https://plugins.svn.wordpress.org/wp-real-estate-property-listing-crm/tags/3.10.16@1172831 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- public/globals/list/default/list-default.php
+++ public/globals/list/default/list-default.php
@@ -34,20 +34,28 @@ Template Name: List - Box Style
 								</a>
 							</h3>
 							<div class="property-amenities">
-								<?php if(!has_filter('list_display_area')){ ?>
+								<?php if(!has_filter('list_display_area_'.md_get_source())){ ?>
 									<span><strong><?php echo md_property_area();?>&nbsp;</strong><?php do_action( 'list_before_area' ); ?><?php echo md_property_area_unit();?></span>
 								<?php } ?>
-								<?php if(!has_filter('list_display_bed')){ ?>
+								<?php if(!has_filter('list_display_bed_'.md_get_source())){ ?>
 									<span><strong><?php echo md_property_beds();?>&nbsp;</strong><?php echo _label('beds');?></span>
 								<?php } ?>
-								<?php if(!has_filter('list_display_baths')){ ?>
+								<?php if(!has_filter('list_display_baths_'.md_get_source())){ ?>
 									<span><strong><?php echo md_property_bathrooms();?>&nbsp;</strong><?php echo _label('baths');?></span>
 								<?php } ?>
-								<?php if(!has_filter('list_display_garage')){ ?>
+								<?php if(!has_filter('list_display_garage_'.md_get_source())){ ?>
 									<span><strong><?php echo md_property_garage();?>&nbsp;</strong><?php echo _label('garage');?></span>
 								<?php } ?>
 							</div>
-							<h2 class="price center-block wp-site-color-theme"><?php echo md_property_price();?></h2>
+							<h2 class="price center-block wp-site-color-theme">
+								<?php
+									if( md_property_raw_price() == 0 ){
+										echo md_property_html_price();
+									}else{
+										echo md_property_format_price();
+									}
+								?>
+							</h2>
 						</div>
 						<div class="panel-footer">
 							<?php
