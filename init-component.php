7a1d739f88edacc76c732d07fe630420e6d0d10f
allan.casilum <allan.casilum@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-29 12:35:44 +0000

    3.27.56
    git-svn-id: https://plugins.svn.wordpress.org/wp-real-estate-property-listing-crm/tags/3.27.56@1209000 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- init-component.php
+++ init-component.php
@@ -11,4 +11,5 @@ require_once plugin_dir_path( __FILE__ ) . 'components/account/init.php';
 require_once plugin_dir_path( __FILE__ ) . 'components/pdf/init.php';
 require_once plugin_dir_path( __FILE__ ) . 'components/propertyalert/init.php';
 require_once plugin_dir_path( __FILE__ ) . 'components/savesearch/init.php';
+require_once plugin_dir_path( __FILE__ ) . 'components/facebookapp/init.php';
 
